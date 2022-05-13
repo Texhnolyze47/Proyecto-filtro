a449cf07517a2a068056e99bd337128405a0c28f
Texhnolyze <ichigobankai136@gmail.com>
2022-05-13 13:56:08 -0500

    add: primera parte del mvc

---
+++ src/controller.py
@@ -0,0 +1,13 @@
+
+from model import Model
+from view import  View
+
+class Controller:
+    def __init__(self):
+        pass
+    def main(self):
+        print('Hola mundo desde el controller')
+
+if __name__ =='__main__':
+    programa = Controller()
+    programa.main()
