6a285c0366f6b3b1a70b4723d13bef8b185db10c
Texhnolyze <ichigobankai136@gmail.com>
2022-06-05 22:47:50 -0500

    se separo el codigo parte 1

--- src/main.py
+++ src/main.py
@@ -1,11 +1,10 @@
 # This is a sample Python script.
 from tkinter import filedialog
 
+from src.conexion import conexion
+from src.ports import *
 import pandas as pd
-import serial
-import serial.tools.list_ports
 import matplotlib.pyplot as plt
-import seaborn as sns
 from tkinter import *
 
 # ruta del archivo
@@ -16,6 +15,7 @@ archivoRuta = []
 
 # creamos la ventana
 ventana = Tk()
+
 
 
 def ventana_boton():
@@ -61,44 +61,6 @@ def abrir_archivo():
     ventana_graficos()
 
 
-# esta funcion simplemente regresa el puerto que usa el arduino para conectarse
-
-
-def get_ports():
-    # esto llama a un objeto con la informacion del arduino
-    ports = serial.tools.list_ports.comports()
-    print('Objeto', ports)
-    # retorna el objecto
-    return ports
-
-
-# esta funcion a va regresar el port que utlizar el arduino
-# esta funcion necesita el objeto
-
-
-def conectar_arduino(ports_encontrados):
-    # variable donde se almacena el port
-    comm_port = "None"
-    # en caso de haya mas de un arduino conectado va a tomar ese numero
-    # es el numero de veces que se va a repetir el ciclo
-    num_connections = len(ports_encontrados)
-
-    for i in range(0, num_connections):
-        # le asignamos otra variable al port
-        port = ports_encontrados[i]
-        print('port antes de hacer cast a un tipo string', port)
-        str_port = str(port)
-        print('port despues de hacer cast a un tipo string', str_port)
-        if 'Arduino' in str_port:
-            # se hace un split para extraer el Com
-            split_port = str_port.split(' ')
-            print('split del string', split_port)
-            # se escoge array que tenga el com en este caso esta en la posicion 0
-            comm_port = (split_port[0])
-            print(comm_port)
-    # returna el COM
-    return comm_port
-
 
 # funcion que grafica
 
@@ -120,14 +82,13 @@ def graficador_searborn():
 
     ruta = ""
     datos = pd.read_excel(ruta.join(archivoRuta))
-    nuevosDatos = datos[['Nodo1','Nodo2','Nodo3','Nodo4','Nodo5','Nodo6','Nodo7','Nodo8','Nodo9','Nodo10','Nodo11','Nodo12',
-    'Nodo13','Nodo14']]
+    nuevosDatos = datos[
+        ['Nodo1', 'Nodo2', 'Nodo3', 'Nodo4', 'Nodo5', 'Nodo6', 'Nodo7', 'Nodo8', 'Nodo9', 'Nodo10', 'Nodo11', 'Nodo12',
+         'Nodo13', 'Nodo14']]
 
     fig = plt.figure()
     plt.plot(nuevosDatos)
     return fig
-
-
 
 
 def encode_send(ard, data):
@@ -146,20 +107,9 @@ def decode_response(ard):
     return respuesta
 
 
-def conexion():
-    try:
-        ser = serial.Serial(port_arduino, 9600, timeout=1)
-        print('Arduino conectado', port_arduino)
-        print('Conexion Hecha')
-    except:
-        print("La conexion fallo")
 
 
 if __name__ == '__main__':
-    # objecto con la info de la conexion de arduino
-    portsEncontrados = get_ports()
-    # variable con el puerto que va utlizar arduino para conectarse con la pc
-    port_arduino = conectar_arduino(portsEncontrados)
 
     conexion()
 
